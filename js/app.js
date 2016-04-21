86262afbbaffc0c9946c6d7f302b8bdf295de311
Christian Ong <ntrdxit@gmail.com>
2016-04-20 21:31:00 -0700

    Coming soon page

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* Theme Name: Moltus - Coming-soon Template
+   Author:Harry
+   Author e-mail: harrynworld@gmail.com
+   Version: 1.0.0
+   Created:January 2015
+   File Description:Main JS file of the template
+*/
+
+//enter the count down date using the format year, month, day, time:time
+$(".count-down").ccountdown(2015,12,31,'23:59');
